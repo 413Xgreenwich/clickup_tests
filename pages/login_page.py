78045ef125b5443e2455c8c48121c76115fd7147
Саня <sanches2101@gmail.com>
2025-05-27 03:03:16 +0300

    login test UI -> DRY

--- pages/login_page.py
+++ pages/login_page.py
@@ -15,12 +15,3 @@ class LoginPage(BasePage):
         self.wait_for_selector_and_type(self.USERNAME_SELECTOR, username, 100)
         self.wait_for_selector_and_type(self.PASSWORD_SELECTOR, password, 100)
         self.wait_for_selector_and_click(self.LOGIN_BUTTON_SELECTOR)
-        self.assert_text_present_on_page("Workspace")
-        self.assert_text_present_on_page("Board")
-
-    def bad_login(self, username, password):
-        self.navigate_to()
-        self.wait_for_selector_and_type(self.USERNAME_SELECTOR, username, 100)
-        self.wait_for_selector_and_type(self.PASSWORD_SELECTOR, password, 100)
-        self.wait_for_selector_and_click(self.LOGIN_BUTTON_SELECTOR)
-        self.assert_text_present_on_page("Incorrect password for this email.")
--- tests/ui/test_login_page.py
+++ tests/ui/test_login_page.py
@@ -10,16 +10,19 @@ class TestLogin:
     def test_success_login(self, sync_browser):
         with allure.step("Создаём новую страницу браузера"):
             page = sync_browser.new_page()
-
         with allure.step("Выполняем логин с корректными данными"):
             login_page = LoginPage(page)
             login_page.login(CLICKUP_EMAIL, CLICKUP_PASSWORD)
+        with allure.step("Проверяем наличие элементов успешного входа"):
+            login_page.assert_text_present_on_page("Workspace")
+            login_page.assert_text_present_on_page("Board")
 
     @allure.description("Проверка ошибки при некорректном пароле")
     def test_fail_login(self, sync_browser):
         with allure.step("Создаём новую страницу браузера"):
             page = sync_browser.new_page()
-
         with allure.step("Выполняем логин с некорректным паролем"):
             login_page = LoginPage(page)
-            login_page.bad_login(CLICKUP_EMAIL, "12345678")
+            login_page.login(CLICKUP_EMAIL, "12345678")
+        with allure.step("Проверяем текст ошибки"):
+            login_page.assert_text_present_on_page("Incorrect password for this email.")
