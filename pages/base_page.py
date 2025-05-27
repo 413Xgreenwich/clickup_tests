c06754f6236807abf560c868269dd6e1d729122e
Саня <sanches2101@gmail.com>
2025-05-27 12:43:58 +0300

    unused BasePage methods deleted

--- pages/base_page.py
+++ pages/base_page.py
@@ -24,26 +24,9 @@ class BasePage:
         self.page.wait_for_selector(selector)
         self.page.click(selector)
 
-    def wait_for_selector_and_fill(self, selector, value):
-        self.page.wait_for_selector(selector)
-        self.page.fill(selector, value)
-
     def wait_for_selector_and_type(self, selector, value, delay):
         self.page.wait_for_selector(selector)
         self.page.type(selector, value, delay=delay)
 
-    def assert_element_is_visible(self, selector):
-        expect(self.page.locator(selector)).to_be_visible()
-
     def assert_text_present_on_page(self, text):
         expect(self.page.locator("body")).to_contain_text(text, timeout=1500000)
-
-    def assert_text_in_element(self, selector, text):
-        expect(self.page.locator(selector)).to_have_text(text)
-
-    def assert_input_value(self, selector, expected_value):
-        expect(self.page.locator(selector)).to_have_value(expected_value)
-
-    def assert_url_is_correct(self, url):
-        self.page.wait_for_load_state("load")
-        expect(self.page).to_have_url(url)
